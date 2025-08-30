256c21c09d78a5e769c67500e09697a105bead8f
Chloe <chloe@sapphic.moe>
2025-08-30 21:20:37 +0000

    feat: use latest linux kernel

--- modules/shared/bootloader.nix
+++ modules/shared/bootloader.nix
@@ -1,11 +1,14 @@
-{ lib, config, ... }:
+{ lib, pkgs, config, ... }:
 {
   options.settings.bootloader.enable = lib.mkEnableOption "Enable bootloader configuration";
 
   config = lib.mkIf config.settings.bootloader.enable {
-    boot.loader = {
-      systemd-boot.enable = true;
-      efi.canTouchEfiVariables = true;
+    boot = {
+      kernelPackages = pkgs.linuxPackages_latest;
+      loader = {
+        systemd-boot.enable = true;
+        efi.canTouchEfiVariables = true;
+      };
     };
   };
 }
