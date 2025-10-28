3d3acdaca4f6f8e6a150241a7aba2074bb611dc9
Chloe <chloe@sapphic.moe>
2025-10-29 03:13:21 +0500

    chore: fix legacy import

--- modules/nixos/hardware/asus.nix
+++ modules/nixos/hardware/asus.nix
@@ -6,7 +6,7 @@
 }:
 
 {
-  config = lib.mkIf config.settings.hardware.laptop.enable {
+  config = lib.mkIf config.settings.profiles.laptop.enable {
     environment.systemPackages = with pkgs; [ asusctl ]; # Control panel for ASUS laptops
 
     services.asusd = {
