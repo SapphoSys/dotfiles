93f02551b911c72bc634c0c1b9bb9b3d19c0c514
Chloe <chloe@sapphic.moe>
2025-10-30 03:45:47 +0500

    chore(bluetooth): change enable logic

--- modules/nixos/hardware/bluetooth.nix
+++ modules/nixos/hardware/bluetooth.nix
@@ -3,13 +3,13 @@
 {
   options.hardware.bluetooth.enable = {
     type = types.bool;
-    default = lib.mkIf lib.stdenv.hostPlatform.isLinux true false;
+    default = lib.stdenv.hostPlatform.isLinux;
     description = ''
       Enable Bluetooth support.
     '';
   };
 
   config = {
-    hardware.bluetooth.enable = lib.mkIf config.hardware.bluetooth.enable;
+    hardware.bluetooth.enable = lib.mkIf config.hardware.bluetooth.enable true;
   };
 }
