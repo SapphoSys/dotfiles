5448bb0bdd37081ba8ea15419760a91d436b6db6
Chloe <chloe@sapphic.moe>
2025-10-30 03:47:43 +0500

    chore(wsl): disable firewall

--- modules/wsl/default.nix
+++ modules/wsl/default.nix
@@ -10,6 +10,9 @@
       enable = true;
       defaultUser = "chloe";
     };
+
+    # Windows manages the firewall for us.
+    settings.firewall.enable = false;
     
     # Allow opening files and links in Windows from WSL
     environment.variables.BROWSER = "wsl-open";
