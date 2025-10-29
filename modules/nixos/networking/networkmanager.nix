861c824edf9a9deca74a3a2c6e0a15426fec2485
Chloe <chloe@sapphic.moe>
2025-10-30 03:48:35 +0500

    chore(networkmanager): unmanaged devices

--- modules/nixos/networking/networkmanager.nix
+++ modules/nixos/networking/networkmanager.nix
@@ -1,5 +1,14 @@
 {
-  networking.networkmanager.enable = true;
+  networking.networkmanager = {
+    enable = true;
+    dns = "systemd-resolved";
+    unmanaged = [
+      "interface-name:tailscale*"
+      "interface-name:docker*"
+      "interface-name:waydroid*"
+      "type:bridge"
+    ];
+  };
 
   systemd.services.NetworkManager-wait-online.enable = false;
 }
