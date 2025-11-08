901b4d8710d1cd303b11fa4f5bdb34dbfba6b540
Chloe <chloe@sapphic.moe>
2025-11-09 01:51:51 +0500

    chore(services:adguardhome): use host and port properties

--- services/adguardhome/default.nix
+++ services/adguardhome/default.nix
@@ -1,15 +1,16 @@
 {
   services.adguardhome = {
     enable = true;
+    host = "0.0.0.0";
+    port = 80;
     settings = {
-      bind_port = 80;
-      bind_host = "0.0.0.0";
       dns = {
         port = 53;
         port_tls = 853;
         port_https = 443;
         port_quic = 784;
       };
+
       web_port = 3000;
     };
   };
