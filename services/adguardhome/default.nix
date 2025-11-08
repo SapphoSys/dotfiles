a50601009f2922a7ecffd50ded1e335888ad33d9
Chloe <chloe@sapphic.moe>
2025-11-09 01:50:34 +0500

    chore(services:adguardhome): fix options

--- services/adguardhome/default.nix
+++ services/adguardhome/default.nix
@@ -1,10 +1,6 @@
 {
   services.adguardhome = {
     enable = true;
-    # Data and config directories (persistent)
-    dataDir = "/var/lib/adguardhome/work";
-    configDir = "/var/lib/adguardhome/conf";
-    # Open all required ports
     settings = {
       bind_port = 80;
       bind_host = "0.0.0.0";
@@ -16,27 +12,17 @@
       };
       web_port = 3000;
     };
-    environment = {
-      TZ = "UTC";
-    };
-    openFirewall = true;
-    # Extra firewall ports for DHCP, etc.
-    extraSettings = {
-      firewall = {
-        allowedTCPPorts = [
-          53
-          80
-          443
-          3000
-          853
-        ];
-        allowedUDPPorts = [
-          53
-          67
-          784
-          853
-        ];
-      };
-    };
+  };
+
+  settings.firewall = {
+    allowedTCPPorts = [
+      80
+      443
+      3000
+    ];
+    allowedUDPPorts = [
+      53
+      784
+    ];
   };
 }
