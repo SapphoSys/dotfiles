b87568abd9b4a4e6070ce80e2f423acd88cb998e
Chloe <chloe@sapphic.moe>
2025-11-12 02:43:06 +0500

    chore(darwin): dock magnification

--- modules/darwin/preferences/dock.nix
+++ modules/darwin/preferences/dock.nix
@@ -2,6 +2,8 @@
   system.defaults.dock = {
     orientation = "bottom";
     show-recents = false;
+    magnification = true;
+    largesize = 72;
     tilesize = 52;
     minimize-to-application = true;
   };
