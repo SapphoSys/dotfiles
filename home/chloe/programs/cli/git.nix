8c0741002a079e5a64c33fe9e90046fe77142daf
Chloe <chloe@sapphic.moe>
2025-11-12 02:41:54 +0500

    chore(git): fix deprecated options

--- home/chloe/programs/cli/git.nix
+++ home/chloe/programs/cli/git.nix
@@ -7,29 +7,35 @@
 
 let
   signingKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICM6XP+CNc2CStEDe/W4LfkcRcG98obQiM2aqnydCRbX";
-  
-  opSshSignPath = 
+
+  opSshSignPath =
     if (osConfig ? wsl) then
       "/mnt/c/Users/Chloe/AppData/Local/1Password/app/8/op-ssh-sign-wsl"
     else if pkgs.stdenv.hostPlatform.isDarwin then
       "/Applications/1Password.app/Contents/MacOS/op-ssh-sign"
-    else 
+    else
       "${pkgs._1password-gui}/bin/op-ssh-sign";
 in
 {
   home.file.".ssh/allowed_signers".text = "* ${signingKey}";
-  
+
   programs.git = {
     enable = true;
-    userName = "Chloe";
-    userEmail = "chloe@sapphic.moe";
 
-    extraConfig = lib.mkMerge [
+    settings = lib.mkMerge [
       {
-        user.signingkey = signingKey;
-        gpg.format = "ssh";
-        gpg.ssh.program = opSshSignPath;
-        gpg.ssh.allowedSignersFile = "~/.ssh/allowed_signers";
+        user = {
+          name = "Chloe A";
+          email = "chloe@sapphic.moe";
+          signingkey = signingKey;
+        };
+
+        gpg = {
+          format = "ssh";
+          ssh.program = opSshSignPath;
+          ssh.allowedSignersFile = "~/.ssh/allowed_signers";
+        };
+
         commit.gpgsign = true;
       }
 
