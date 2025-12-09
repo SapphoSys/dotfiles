8bc16947ee3105911e063b7fc7c709355588485b
Chloe A <chloe@sapphic.moe>
2025-12-09 05:50:04 +0500

    fix(services:minecraft): try adding overlay

--- flake.nix
+++ flake.nix
@@ -99,6 +99,7 @@
         shared = {
           modules = [ ./modules/base ];
           specialArgs = { inherit inputs; };
+          nixpkgs.overlays = [ inputs.nix-minecraft.overlay ];
         };
 
         perClass = class: {
